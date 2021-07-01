7ff5f0171c81ef212c05d3cc786e75d9ad8c1fca
Benjamin Scharf <benjamin.d.scharf@gmail.com>
2021-07-01 13:48:39 -0500

    Check for variables shadowed by for loop assignments.

--- flake8_loopy/defs.py
+++ flake8_loopy/defs.py
@@ -1,8 +1,8 @@
 from typing import List, Tuple
 
 # List of variable name, is_used pairs in current context
-ContextVars = List[Tuple[str, bool]]
+ContextVarTracker = List[Tuple[str, bool]]
 
 # Holds unused variable error
 # line number, column offset, error code, var name
-UnusedVarError = Tuple[int, int, int, str]
+ErrorMessage = Tuple[int, int, int, str]
